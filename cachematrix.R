80c750d80005938f9adb242c777cfa272d8520c3
alehnert <alehnert@gmail.com>
2015-06-19 12:52:23 -0700

    Completed Assignment 2
    contains explanation and two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming: Second programming assignment
+## created by alehnert, June 19, 2015
+## contains two functions that (1) creates a special matrix that can cache its inverse and
+## (2) computes the inverse of the special matrix if it does not already exist in cache
 
-## Write a short comment describing this function
+## Note: functions must be run in order, for example:
+## > f1<-makeCacheMatrix(matrix(c(2,2,3,2),2,2))
+## > cacheSolve(f1)  
+## ---> returns inverse of matrix(c(2,2,3,2),2,2)
+## > cacheSolve(f1)  
+## ---> returns cached inverse of the matrix
 
+
+## Creates a list of functions ("special matrix") that can cache the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  im<-NULL
+  set <- function(y){
+    x <<- y
+    im <<- NULL
+  }
+  get <-function()x
+  setinverse <- function(solve) im <<- solve
+  getinverse <- function() im
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Computes the inverse of a matrix after checking if its inverse is cached
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getinverse()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data)
+  x$setinverse(im)
+  im  
 }
